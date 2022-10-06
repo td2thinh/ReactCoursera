63f535768579dc1c5716856da522a82313cc9444
td2thinh <75013885+td2thinh@users.noreply.github.com>
2022-10-06 23:40:35 +0700

    Components Part 1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
+
   );
 }
 
